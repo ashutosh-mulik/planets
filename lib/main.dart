f8ce5bb6b491dbdd8d3bb5cd20d8efd0984ceb59
Ashutosh <ashutoshmulik@duck.com>
2022-03-07 17:46:51 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Planets',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Main(),
+    );
+  }
+}
+
+class Main extends StatelessWidget {
+  const Main({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
